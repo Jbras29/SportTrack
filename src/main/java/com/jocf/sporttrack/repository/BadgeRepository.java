08bc2a9193015221c9189f78471a823f5005ec06
Jbras29 <julienbras0@gmail.com>
2026-04-07 17:12:55 +0200

    little bug fix

--- src/main/java/com/jocf/sporttrack/repository/BadgeRepository.java
+++ src/main/java/com/jocf/sporttrack/repository/BadgeRepository.java
@@ -2,16 +2,13 @@ package com.jocf.sporttrack.repository;
 
 
 import com.jocf.sporttrack.model.Badge;
-import com.jocf.sporttrack.model.Utilisateur;
-import org.springframework.data.domain.Example;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
-import java.util.Optional;
 
 @Repository
-public interface BadgeRepository extends JpaRepository<Utilisateur, Long> {
+public interface BadgeRepository extends JpaRepository<Badge, Long> {
 
     List<Badge> findByKmNecessaireLessThanEqual(int distanceEnKm);
 
