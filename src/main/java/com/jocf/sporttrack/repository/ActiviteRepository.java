e7c9b6cf592694fd96f34514c591e0e9c897291f
Ousmane199 <cisseousmane3208@gmail.com>
2026-04-07 16:07:46 +0200

    Modification du code

--- src/main/java/com/jocf/sporttrack/repository/ActiviteRepository.java
+++ src/main/java/com/jocf/sporttrack/repository/ActiviteRepository.java
@@ -5,5 +5,4 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ActiviteRepository extends JpaRepository<Activite, Long> {
 
-}</content>
-<parameter name="filePath">c:\Users\cisse\Desktop\projetFin2026\SportTrack\src\main\java\com\jocf\sporttrack\repository\ActiviteRepository.java
+}
