3d064624795c073c88330126ce09779d2dfe08a1
Jbras29 <julienbras0@gmail.com>
2026-04-07 13:08:58 +0200

    init

---
+++ src/test/java/com/jocf/sporttrack/SporttrackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jocf.sporttrack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SporttrackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
