ec8d8af93a9589a9a097ea3e8862923a02fd4cfb
27-feno <ralitera261@gmail.com>
2026-04-08 15:47:31 +0200

    utilisateur inexistant

--- src/main/java/com/jocf/sporttrack/repository/UtilisateurRepository.java
+++ src/main/java/com/jocf/sporttrack/repository/UtilisateurRepository.java
@@ -1,6 +1,8 @@
 package com.jocf.sporttrack.repository;
 
 import com.jocf.sporttrack.model.Utilisateur;
+
+import java.util.List;
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,4 +13,5 @@ public interface UtilisateurRepository extends JpaRepository<Utilisateur, Long> 
     Optional<Utilisateur> findByEmail(String email);
 
     boolean existsByEmail(String email);
+    List<Utilisateur> findByPrenomContainingIgnoreCase(String prenom);
 }
