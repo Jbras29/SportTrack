0a5f8ef7242e45cb894dbdf753ff9ed89c6603be
Ousmane199 <cisseousmane3208@gmail.com>
2026-04-08 13:51:30 +0200

    Lets goooooooooooo

--- src/main/java/com/jocf/sporttrack/controller/LoginController.java
+++ src/main/java/com/jocf/sporttrack/controller/LoginController.java
@@ -4,11 +4,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.Operation;
+
 @Controller
 public class LoginController {
 
     @GetMapping("/login")
     public String loginPage() {
         return "login"; 
+    }
+
+    @GetMapping("/register") 
+    public String registerPage() {
+        return "/create/register";
     }
 }
